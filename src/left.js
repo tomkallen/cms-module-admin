427f8c1e2f2d86b74ce62d79a3e962d1f29b8266
Alex Bykov <spbeat@mail.ru>
2018-02-11 22:26:37 +0300

    router

---
+++ src/left.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Left extends Component {
+  render() {
+    return <div className="Left">Left </div>;
+  }
+}
+
+export default Left;
